24d73a27d4292489bb4e9dc5242fac238fb15cdb
Juyeon Lee <lqu134@naver.com>
2023-10-16 23:25:03 +0900

    find max number completed

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(number) {
+    let max = number[0]
+    for(let i=1; i<number.length; i++){
+        if(max < number[i]){
+            max=number[i]
+        }
+    }
+    return max
+}
 
 module.exports = findMaxNumber;
