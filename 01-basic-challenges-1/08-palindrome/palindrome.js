55be4e5ee2fdac49cdffbee756c0f0cb3fc13ace
Juyeon Lee <lqu134@naver.com>
2023-10-27 13:47:10 +0900

    practised palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,24 @@
-function isPalindrome() {}
+function isPalindrome(string) {
+    let checkedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let words = ""
+    let reversed =""
+    for(let i=0; i<checkedString.length; i++){
+        words += checkedString[i]
+    }
+    for(let i=checkedString.length-1; i>=0; i--){
+        reversed += checkedString[i]}
+    if (words === reversed) {
+      return true;
+    } else {
+      return false;
+    }
+    
+}
+
+// function isPalindrome(str){
+//     const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g,'')
+//     const reversedStr = formattedStr.split('').reverse().join('')
+//     return formattedStr === reversedStr
+// }
 
 module.exports = isPalindrome;
